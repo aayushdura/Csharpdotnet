d3dc1aafd2dc4d098b6a34b198f8e7dc78b27553
AaYuSh <58044533+aayushdura@users.noreply.github.com>
2021-09-03 19:35:49 +0545

    before pracical

--- localrepo/EmployeeManagement/Controllers/EmployeeController.cs
+++ localrepo/EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,9 +1,37 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 public class EmployeeController: Controller
 {
     public ActionResult Index()
     {
-        return View();
+        person emp1= new person()
+        {
+            Name = "Aayush Dura",
+            address="Satungal",
+            Salary=20000
+        };
+        person emp2= new person()
+        {
+            Name = "Kishan Dura",
+            address="Rambazar",
+            Salary= 32000
+        };
+        person emp3= new person()
+        {
+            Name = "Nischal Shrestha",
+            address="talchikhel",
+            Salary= 32000
+        };
+         person emp4= new person()
+        {
+            Name = "Sabita rai",
+            address="satungal",
+            Gender='F',
+            Salary= 32000
+        };
+         var employee= new List<person>(){emp1,emp2,emp3,emp4};
+        return View(employee);
     }
 
 }
---
+++ localrepo/EmployeeManagement/Models/employee.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class person
+{  
+    public string Name { get; set; }
+    public string address { get; set; }
+    public char Gender { get; set; }= 'M';
+    public int Salary { get; set; }
+    
+}
